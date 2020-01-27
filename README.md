1dcc8576328b614d0e45c9c6e9d0a4de514b07ea
Abram Symons <abramsymons@protonmail.com>
2020-01-27 09:26:36 +0330

    first code

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# BrightID-Node-Backup-Script
-Script for official BrightID nodes to upload daily backups
+# BrightID-Node Backup Script
+
+This script is used by the official brightid nodes to upload daily backups to the google storage service.
+Other nodes that want to join the network restore the last backup to reach the state of the backup time. Nodes read the operations sent after the backup time from the blockchain and apply them to sync to the current state of the network.
+
+Latest BrightID backup can always be downloaded from:
+
+`https://storage.googleapis.com/brightid-backups/brightid.tar.gz`
+
+Available backup files can be found:
+
+`https://storage.googleapis.com/brightid-backups/`
+
+There are backup files avilable for following times:
+
+- one file per day for a week
+- one file per week for a month
+- one file per month forever
+
+### Prerequisites:
+
+- python3
+- pip3
+- arangodump
+
+* You can find out how to install `arangodump` from arango client tools on your os from [here](https://www.arangodb.com/download-major/).
+
+### Install
+
+`pip3 install -r requirements.txt`
+
+### Run
+
+- Place your google credentials json file beside the `backup.py` with the `google.json` as name.
+
+- Add a cron job to run the script everyday.
+
+```
+# m h  dom mon dow   command
+0 1 * * * python3 /path/to/script/backup.py
+```
---
+++ backup.py
@@ -0,0 +1,61 @@
+import os
+import requests
+from datetime import date, datetime, timedelta
+from google.cloud import storage
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(dir_path, 'google.json')
+
+BUCKET_NAME = 'brightid-backups'
+BACKUP_CMD = ' && '.join([
+    'rm /tmp/dump -rf',
+    'arangodump --compress-output false --collection variables --collection usersInGroups --collection usersInNewGroups --collection groups --collection sponsorships --collection newGroups --collection operationsHashes --collection users --collection variables --collection connections --server.password "" --output-directory "/tmp/dump"',
+    'cd /tmp',
+    'tar -zcvf brightid.tar.gz dump'
+])
+
+def upload(fpath):
+    print('Uploading brightid.tar.gz')
+    client = storage.Client()
+    bucket = client.get_bucket(BUCKET_NAME)
+    fname = os.path.basename(fpath)
+    blob = bucket.blob(fname)
+    with open(fpath, 'rb') as f:
+        blob.upload_from_file(f)
+    # copy the brightid.tar.gz to brightid_yymmdd.tar.gz
+    date_str = date.today().strftime('%y%m%d')
+    fname_with_date = fname.replace('.tar.gz', '_{}.tar.gz'.format(date_str))
+    print('Copying brightid.tar.gz to {}'.format(fname_with_date))
+    bucket.copy_blob(blob, bucket, fname_with_date)
+
+def get_date(fname):
+    s = fname.strip('brightid_').strip('.tar.gz')
+    return datetime.strptime(s, '%y%m%d').date()
+
+def delete_extra_files():
+    print('Deleting extra files')
+    client = storage.Client()
+    blobs = client.list_blobs(BUCKET_NAME)
+    blobs = {blob.name: blob for blob in blobs if blob.name.startswith('brightid_')}
+    fnames = blobs.keys()
+    today = date.today()
+    for fname in fnames:
+        days = (today - get_date(fname)).days
+        keep = False
+        # keep one file per day for a week
+        if days < 7:
+            keep = True
+        # keep one file per week for a month
+        elif days < 30 and get_date(fname).weekday() == 1:
+            keep = True
+        # keep one file per month forever
+        elif get_date(fname).day == 1:
+            keep = True
+        if not keep:
+            print('delete {} from cloud'.format(fname))
+            blobs[fname].delete()
+
+if __name__ == '__main__':
+    assert os.system(BACKUP_CMD)==0, 'backup failed'
+    upload('/tmp/brightid.tar.gz')
+    delete_extra_files()
