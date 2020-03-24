d8308ee515e271e8c37b7315ea0317cbfc4005c8
Abram Symons <abramsymons@gmail.com>
2020-03-24 11:09:48 +0430

    add verifications to users info

--- backup.py
+++ backup.py
@@ -83,7 +83,7 @@ def load_json():
     ret = {'nodes': [], 'edges': []}
     buf = {}
     for u in users:
-        users[u] = {'rank': users[u]['score'], 'id': u , 'groups': []}
+        users[u] = {'rank': users[u]['score'], 'id': u , 'groups': [], 'verifications': users[u]['verifications']}
         ret['nodes'].append(users[u])
     for user_group in user_groups.values():
         u = user_group['_from'].replace('users/', '')
